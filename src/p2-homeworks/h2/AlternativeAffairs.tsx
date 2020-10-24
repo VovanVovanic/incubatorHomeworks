5e7f8ce4793eb90c347f7bd61b2fd6acce84eb99
Vladimir <vladimirplotnikov371@gmail.com>
2020-10-24 20:16:07 +0300

    hw2 done

--- src/p2-homeworks/h2/AlternativeAffairs.tsx
+++ src/p2-homeworks/h2/AlternativeAffairs.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable no-lone-blocks */
 import React, { Component } from "react";
 import BookItem from "./BookItem";
-import Buttons from './Buttons'
-import SearchBook from './Search'
+import Buttons from "./Buttons";
+import SearchBook from "./Search";
 import classes from "./Affairs.module.css";
 
 ///// Game of thrones API
 export type responseType = {
   publisher: string;
-name: string;
-  id: number
+  name: string;
+  id: number;
 };
 
 export type bookStateType = {
-  bookList: Array<responseType>
-  filter: string
-  search: string
-}
+  bookList: Array<responseType>;
+  filter: string;
+  search: string;
+};
 export type BookFilterType = string;
 
 const getBooks = async (url: string) => {
@@ -25,15 +25,19 @@ const getBooks = async (url: string) => {
     throw new Error("Wow! something gonna wrong");
   }
   const booksInfo = await res.json();
-    let books = booksInfo.map((el: responseType, i:number) => ({ name: el.name, publisher: el.publisher, id: i }));
-    return books
+  let books = booksInfo.map((el: responseType, i: number) => ({
+    name: el.name,
+    publisher: el.publisher,
+    id: i,
+  }));
+  return books;
 };
 
 export default class AlternativeAffairs extends Component {
   state: bookStateType = {
     bookList: [],
     filter: "all",
-    search: ''
+    search: "",
   };
 
   componentDidMount() {
@@ -43,6 +47,7 @@ export default class AlternativeAffairs extends Component {
       })
     );
   }
+
   getIndex(arr: Array<any>, i: number) {
     return arr.findIndex((el: responseType) => el.id === i);
   }
@@ -54,7 +59,6 @@ export default class AlternativeAffairs extends Component {
       bookList: [...list.slice(0, index), ...list.slice(index + 1)],
     });
   };
-
 
   onFiltered = (arr: Array<any>, filter: string) => {
     switch (filter) {
@@ -83,35 +87,41 @@ export default class AlternativeAffairs extends Component {
         }
         break;
 
-      case 'Tor Publishing': {
-        return arr.filter((el: responseType) => el.publisher === "Tor Fantasy" || el.publisher === "Tor Books")
-      }
+      case "Tor Publishing":
+        {
+          return arr.filter(
+            (el: responseType) =>
+              el.publisher === "Tor Fantasy" || el.publisher === "Tor Books"
+          );
+        }
         break;
-      default: 
-        return arr
+      default:
+        return arr;
     }
   };
+
   onChangeFilter = (filter: string) => {
     this.setState({
-      filter
-    })
-  }
+      filter,
+    });
+  };
 
   onSearch = (arr: Array<any>, search: string) => {
     if (!search) {
-      return arr
-    }
-    else {
+      return arr;
+    } else {
       return arr.filter((el: responseType) => {
-        return el.name.toLowerCase().indexOf(search.toLowerCase())>1
-      })
+        return el.name.toLowerCase().indexOf(search.toLowerCase()) > 1;
+      });
     }
-  }
+  };
+
   onSearchChange = (search: string) => {
     this.setState({
-      search
-    })
-  }
+      search,
+    });
+  };
+
   renderBookList(arr: Array<responseType>) {
     return arr.map((el: responseType, i: number) => {
       return (
@@ -128,25 +138,21 @@ export default class AlternativeAffairs extends Component {
   }
   render() {
     const { bookList, filter, search } = this.state;
-    const searchedBook = this.onSearch(bookList, search)
-    const filteredBooks = this.onFiltered(searchedBook, filter)
+    const searchedBook = this.onSearch(bookList, search);
+    const filteredBooks = this.onFiltered(searchedBook, filter);
 
-    let isDataLoaded = bookList ? this.renderBookList(filteredBooks) : "...loaded";
+    let isDataLoaded = bookList
+      ? this.renderBookList(filteredBooks)
+      : "...loaded";
 
     return (
       <>
-        <SearchBook
-        onSearchChange={this.onSearchChange}
-        
-        />
+        <SearchBook onSearchChange={this.onSearchChange} />
         <ul className={classes.affairList}>{isDataLoaded}</ul>
         <div className={classes.wrapper}>
           <p>Sort by publisher</p>
           <div className={classes.sorted}>
-            <Buttons
-              filter={filter}
-              onClick={this.onChangeFilter}           
-            />
+            <Buttons filter={filter} onClick={this.onChangeFilter} />
           </div>
         </div>
       </>

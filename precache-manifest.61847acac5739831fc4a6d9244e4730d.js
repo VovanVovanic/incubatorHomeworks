d18e20b43a4af1da0e02fe214017f0383f8cec8d
Vladimir <vladimirplotnikov371@gmail.com>
2020-11-21 18:44:38 +0200

    Updates

---
+++ precache-manifest.61847acac5739831fc4a6d9244e4730d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a820d4fdaf9334d33163be5ad04fd087",
+    "url": "/incubatorHomeworks/index.html"
+  },
+  {
+    "revision": "0d513648cb58c949b59e",
+    "url": "/incubatorHomeworks/static/css/main.d19e8211.chunk.css"
+  },
+  {
+    "revision": "dd1731b9f4eda9f665cc",
+    "url": "/incubatorHomeworks/static/js/2.978613be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/incubatorHomeworks/static/js/2.978613be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d513648cb58c949b59e",
+    "url": "/incubatorHomeworks/static/js/main.67bce877.chunk.js"
+  },
+  {
+    "revision": "e34beb10c352c69d72c1",
+    "url": "/incubatorHomeworks/static/js/runtime-main.81915009.js"
+  },
+  {
+    "revision": "7b3cd18c393fb84be4d6cdb7d2e577bb",
+    "url": "/incubatorHomeworks/static/media/simon.7b3cd18c.jpg"
+  }
+]);
